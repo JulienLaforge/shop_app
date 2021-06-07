7934be7d732dcdcd75a48f32e39f688564d81133
Julien <julien.a.laforge@gmail.com>
2021-06-07 10:41:53 +0200

    provider products listeners

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -42,6 +42,10 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(id) {
+    return _items.firstWhere((item) => item.id == id);
+  }
+
   void addProduct() {
     // _items.add(value);
     notifyListeners();
