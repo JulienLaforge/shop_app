10889583046334cf1a648816726b4eb9abdbd1a0
Julien <julien.a.laforge@gmail.com>
2021-06-07 15:20:11 +0200

    provider optimization

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,10 +13,9 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (context, index) => ProductItem(
-        id: products[index].id,
-        title: products[index].title,
-        imageUrl: products[index].imageUrl,
+      itemBuilder: (context, index) => ChangeNotifierProvider.value(
+        value: products[index],
+        child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
