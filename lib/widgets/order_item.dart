7ff4ccb4ab17be33df7f9b44acb5c690bfbcd388
Julien <julien.a.laforge@gmail.com>
2021-06-08 20:40:13 +0200

    using an app drawer

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -17,7 +17,7 @@ class OrderItem extends StatelessWidget {
           ListTile(
             title: Text('${order.amount} €'),
             subtitle: Text(
-              DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(order.dateTime),
             ),
             trailing: IconButton(
               icon: Icon(
